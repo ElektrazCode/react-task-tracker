c2db0c9d99884f52713402ac6bf91f9e08cd89c0
ElektrazCode <ElektrazCode@gmail.com>
2022-10-15 11:37:08 -0400

    Added Header Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+
+import Header from './components/Header'
+
 function App() {
   return (
-    <div className="App">
-     <h1>Hello from React</h1>
+    <div className="container">
+      <Header />
     </div>
   );
 }
+
 
 export default App;
