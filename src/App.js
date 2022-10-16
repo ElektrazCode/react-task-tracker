d8c341785b6e0a758a9feaac611d75099b318342
ElektrazCode <ElektrazCode@gmail.com>
2022-10-15 21:04:57 -0400

    Added About link but still needs debugging

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
-import { useState, useEffect } from 'react'
+import Footer from './components/Footer'
+import About from './components/About'
+
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -58,14 +61,24 @@ const App = () => {
   }
 
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd = {showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask}/>}
-      {tasks.length > 0 ? 
-        <Tasks tasks = {tasks} onDelete = {deleteTask} onToggle={toggleReminder}/>
-                        : 'No Tasks'}
-    </div>
-  );
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd = {showAddTask} />
+          <Routes>
+            <Route path='/' exact render= {(props)=> (
+              <>
+               {showAddTask && <AddTask onAdd={addTask}/>}
+                {tasks.length > 0 ? 
+                <Tasks tasks = {tasks} onDelete = {deleteTask} onToggle={toggleReminder}/>
+                            : 'No Tasks'}
+              </>
+            )}/>
+            <Route path='/about' component={About} />
+          </Routes>
+        <Footer />
+      </div>
+    </Router>
+  )
 }
 
 
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+// import { Link } from 'react-router-dom'
+const footer = () => {
+  return (
+    <footer>
+        <p>Copyright &copy; 2022</p>
+        <a href="/about">About</a>
+    </footer>
+  )
+}
+
+export default footer
